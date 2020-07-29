3f56b5a27c986bfd8955a57ffbb0322a9f39bcd8
kyrylo.karnyshov@gmail.com <Hub0ctObus1>
2020-07-29 21:23:23 +0300

    added tests for login and sign up

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,12 +5,34 @@ import pytest
 class TestLogin(Template):
 
     def test_login(self):
-        pass
+        response = self.api.post(self.api.LOGIN_URL, json={
+            'email': 'eve.holt@reqres.in',
+            'password': 'cityslicka'
+        })
+
+        assert 200 == response.status_code
+        assert "QpwL5tke4Pnpja7X4" == response.json()['token']
 
     @pytest.mark.xfail
     def test_login_negative_password(self):
-        pass
+        response = self.api.post(self.api.LOGIN_URL, json={'email': 'eve.holt@reqres.in'})
+
+        assert 400 == response.status_code
+        assert "Missing password" == response.json()['error']
 
     @pytest.mark.xfail
     def test_login_negative_email(self):
-        pass
+        response = self.api.post(self.api.LOGIN_URL, json={'password': 'cityslicka'})
+
+        assert 400 == response.status_code
+        assert "Missing email or username" == response.json()['error']
+
+    @pytest.mark.xfail
+    def test_login_no_user(self):
+        response = self.api.post(self.api.LOGIN_URL, json={
+            'email': 'test',
+            'password': '123'
+        })
+
+        assert 400 == response.status_code
+        assert "user not found" == response.json()['error']
--- tests/test_signup.py
+++ tests/test_signup.py
@@ -5,12 +5,25 @@ import pytest
 class TestSignUp(Template):
 
     def test_signup(self):
-        pass
+        response = self.api.post(self.api.REGISTER_URL, json={
+            'email': 'eve.holt@reqres.in',
+            'password': 'pistol'
+        })
+
+        assert 200 == response.status_code
+        assert 4 == response.json()['id']
+        assert "QpwL5tke4Pnpja7X4" == response.json()['token']
 
     @pytest.mark.xfail
     def test_signup_negative_password(self):
-        pass
+        response = self.api.post(self.api.REGISTER_URL, json={'email': 'eve.holt@reqres.in'})
+
+        assert 400 == response.status_code
+        assert "Missing password" == response.json()['error']
 
     @pytest.mark.xfail
     def test_signup_negative_email(self):
-        pass
+        response = self.api.post(self.api.REGISTER_URL, json={'password': 'pistol'})
+
+        assert 400 == response.status_code
+        assert "Missing email or username" == response.json()['error']
